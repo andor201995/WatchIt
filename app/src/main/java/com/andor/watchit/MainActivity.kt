f248e1687f069cbedf7ee95049337abe9baa1253
Anmol Srivastava <andor201995@users.noreply.github.com>
2020-01-11 15:16:48 +0530

    init app with empty application

---
+++ app/src/main/java/com/andor/watchit/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.andor.watchit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
