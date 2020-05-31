984650386e5208b6c8c0a3d7e7463a041cd2fb20
Anmol Srivastava <andor201995@users.noreply.github.com>
2020-05-31 21:26:26 +0530

    Handling nav options

--- app/src/main/java/com/andor/watchit/screens/common/controller/main/MainActivity.kt
+++ app/src/main/java/com/andor/watchit/screens/common/controller/main/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.andor.watchit.screens.common.controller.main
 
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.andor.watchit.R
+import com.andor.watchit.core.inVisible
+import com.andor.watchit.core.visible
 import com.andor.watchit.screens.common.controller.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,7 +21,36 @@ class MainActivity : BaseActivity() {
             this,
             navController
         )
+        setupBottomNavViewAndFAB(navController)
+    }
+
+    private fun setupBottomNavViewAndFAB(navController: NavController) {
         btm_nav_bar.setupWithNavController(navController)
+        btm_nav_bar.isItemHorizontalTranslationEnabled = true
+        btm_nav_bar.setOnNavigationItemReselectedListener {
+            // do nothing when reselected
+        }
+
+        searchFAB.setOnClickListener {
+            navController.navigate(R.id.searchMovieFragment)
+        }
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.topRatedMovieListFragment -> showBottomNav()
+                else -> hideBottomNav()
+            }
+        }
+    }
+
+    private fun hideBottomNav() {
+        btm_nav_bar.inVisible()
+        searchFAB.inVisible()
+    }
+
+    private fun showBottomNav() {
+        btm_nav_bar.visible()
+        searchFAB.visible()
     }
 
     override fun getLayoutRes() = R.layout.activity_main
